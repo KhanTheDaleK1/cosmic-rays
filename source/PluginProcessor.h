2210f23c10cb1d7bcd56f4e1ab50d1726ad2e543
KhanTheDaleK1 <evan.t.beechem@gmail.com>
2026-03-19 22:55:39 -0500

    fix: disable LTO globally to prevent segfaults in 'juce_lv2_helper' and 'juce_vst3_helper'; update build process for Linux

--- source/PluginProcessor.h
+++ source/PluginProcessor.h
@@ -22,6 +22,7 @@
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_gui_basics/juce_gui_basics.h>
 #include <juce_audio_utils/juce_audio_utils.h>
+#include "Version.h"
 #include "GranularEngine.h"
 
 class CosmicRaysAudioProcessor  : public juce::AudioProcessor
